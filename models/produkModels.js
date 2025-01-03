ec0c24ffb62bf1314efce398f103930f5a6adf5f
Istiqomah <127840127+istiqomahi@users.noreply.github.com>
2025-01-03 15:15:03 +0700

    Add files via upload

---
+++ models/produkModels.js
@@ -0,0 +1,46 @@
+import { Sequelize } from "sequelize";
+import db from "../config/Database.js";
+import Users from "./userModels.js"; 
+
+const {DataTypes} = Sequelize;
+
+const produkModels = db.define('product',{
+    uuid:{
+        type: DataTypes.STRING,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        validate:{
+            notEmpty: true
+        }
+    },
+    name:{
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate:{
+            notEmpty: true,
+            len: [3, 100]
+        }
+    },
+    price:{
+        type:DataTypes.INTEGER,
+        allowNull: false,
+        validate:{
+            notEmpty: true,
+        }
+    },
+    userId:{
+        type:DataTypes.INTEGER,
+        allowNull: false,
+        validate:{
+            notEmpty: true,
+        }
+    },
+},{
+    freezeTableName: true
+});
+
+Users.hasMany(produkModels);
+produkModels.belongsTo(Users, {
+    foreignKey: 'userId'
+});
+export default produkModels;
